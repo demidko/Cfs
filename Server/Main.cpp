78e0e47b681d77a2c8af42f973449ed4ab52b5bd
Daniel Demidko <DanielDemidko@gmail.com>
2019-06-14 13:16:32 +1000

    init

--- Server/Main.cpp
+++ Server/Main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 #include "Server.h"
 
+#include <sstream>
+
+std::string ExtractBody(const std::string_view s)
+{
+    const int fix = s.find("\n\r");
+    if (fix == -1)
+    {
+        return {};
+    }
+    return std::string(s).substr(fix, 12);
+}
+
 int main()
 {
-    Server::Start([](const std::string_view requestBody)
+    Server::Start([](const std::string_view req)
         {
-            return std::string(requestBody);
+            return ExtractBody(req);
         });
 }
